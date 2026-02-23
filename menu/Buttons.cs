2ecc0ec706696ae51d2f5bfc05c8d1b7336e8575
grindlegl-wq <grindlegl@gmail.com>
2026-02-23 08:14:14 -0500

    Add files via upload

---
+++ menu/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using StupidTemplate.Classes;
+using static StupidTemplate.Menu.Main;
+using static StupidTemplate.Settings;
+
+namespace StupidTemplate.Menu
+{
+    public class Buttons
+    {
+        /*
+         * Here is where all of your buttons are located.
+         * To create a button, you may use the following code:
+         * 
+         * Move to Category:
+         *   new ButtonInfo { buttonText = "Settings", method =() => currentCategory = 1, isTogglable = false, toolTip = "Opens the main settings page for the menu."},
+         *   new ButtonInfo { buttonText = "Return to Main", method =() => currentCategory = 0, isTogglable = false, toolTip = "Returns to the main page of the menu."},
+         * 
+         * Togglable Mod:
+         *   new ButtonInfo { buttonText = "Platforms", method =() => Movement.Platforms(), toolTip = "Spawns platforms on your hands when pressing grip."},
+         */
+
+        public static ButtonInfo[][] buttons = new ButtonInfo[][]
+        {
+            new ButtonInfo[] {
+                new ButtonInfo { buttonText = "Disconnect", method =() => NetworkSystem.Instance.ReturnToSinglePlayer(), isTogglable = false, toolTip = "Disconnects you from the room."},
+            },
+        };
+    }
+}
